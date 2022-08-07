78cb53ff241cf525e7535eba284dafb6c186976b
Parvat R <vidyutparvat2@gmail.com>
2022-08-07 11:49:33 +0530

    Fixed nav, added transition to loader

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 alert("Page is not completed yet. Lol")
 
 $(document).ready(function() {
-    setTimeout(()=>{$("#loading").addClass("hide");}, 4000)
-    $(".name").removeClass("hide")
+    setTimeout(()=>{$("#loading").addClass("hide");}, 4000);
+
+    $(".name").removeClass("hide");
+    
+    $(".toggler").click(()=> {
+        $("#nav").toggleClass("visible");
+    })
 })
