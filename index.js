e3af30aa18559c047292792ce3ab6fba0106d508
Parvat R <vidyutparvat2@gmail.com>
2022-08-19 17:22:20 +0530

    idk

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-
-alert("Page is not completed yet. Lol")
-
 $(document).ready(function() {
-    setTimeout(()=>{$("#loading").addClass("hide");}, 4000);
-
+    setTimeout(()=>{$("#loading").addClass("hide");}, 1000);
     $(".name").removeClass("hide");
-    
     $(".toggler").click(()=> {
         $("#nav").toggleClass("visible");
     })
