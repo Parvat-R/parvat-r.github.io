14fbe53d1ff41a79a6bc23538cc823c8f194dcfb
Parvat R <vidyutparvat2@gmail.com>
2022-08-27 13:42:14 +0530

    index.html's css done i guess

--- index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    setTimeout(()=>{$("#loading").addClass("hide");}, 1000);
-    $(".name").removeClass("hide");
-    $(".toggler").click(()=> {
-        $("#nav").toggleClass("visible");
-    })
-})
